79e8c22f0f994d0e28bd2c4323c246cbf0d1f740
Alina <alinkartemenko@mail.ru>
2022-06-29 22:09:31 +0300

    add ui material

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,13 +3,14 @@ import classnames from 'classnames';
 
 import styles from './Item.module.css';
 
-const Item = ({ value, isDone }) => (<span className={
-    classnames({
-      [styles.item]: true,
-      [styles.done]: isDone
-    })
-}>    
-    {value}
-</span>);
+const Item = ({ value, isDone }) => (
+  <span className={
+      classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+      })
+  }>    
+      {value}
+  </span>);
 
 export default Item;
