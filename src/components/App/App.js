094e41f9779efbe285fcdf2f781faa75da4d315f
Alina <alinkartemenko@mail.ru>
2022-08-28 17:08:45 +0300

    do refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,30 +2,42 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 import styles from './App.module.css';
 
-const App = () => {
-    const items = [
-      {
-          value: 'Create new app',
-          isDone: true
-      },
-      {
-          value: 'Finish that lesson',
-          isDone: false
-      },
-      {
-          value: 'Listen webinar',
-          isDone: false
-      }
-    ];
-
+class App extends React.Component {
+    state = {
+        items: [
+            {
+              value: 'Create new app',
+              isDone: true
+            },
+            {
+              value: 'Finish that lesson',
+              isDone: false
+            },
+            {
+              value: 'Listen webinar',
+              isDone: false
+            }
+        ]
+    };
+  
+  render() {
     return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>todos</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count={3} />
-    </div>);
-}
+      <div className={styles.wrap}>
+        <Card>
+          <CardContent>
+            <h1 className={styles.title}>todos</h1>
+            <InputItem />
+            <ItemList items={this.state.items} />
+            <Footer count={3} />
+          </CardContent>
+        </Card>
+      </div>);
+    } 
+};
+
 export default App;
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
 import styles from './Item.module.css';
 
-const Item = ({ value, isDone }) => (
-  <span className={
-      classnames({
-        [styles.item]: true,
-        [styles.done]: isDone
-      })
-  }>    
-      {value}
-  </span>);
 
+class Item extends React.Component {
+
+  render() {
+    const {value, isDone, onClickDone, id, onClickDelete} = this.props;
+    return <span className={
+          classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+          })
+    }>
+  <span>
+    <Checkbox
+      checked={isDone}
+      tabIndex={-1}
+      onClick={() => onClickDone(id)}
+    />
+    {value}
+  </span>
+    <IconButton
+      className={styles.delete}
+      aria-label="delete"
+      onClick={() => onClickDelete(id)}
+    >
+      <DeleteIcon />
+    </IconButton>  
+  </span>}
+}
+
+Item.propTypes = {
+  value: PropTypes.string.isRequired,
+  isDone: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+  onClickDone: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+}
 export default Item;
